a6471697275d91d43b211dceb49b570b46498c2b
Martin Juckes <martin.juckes@stfc.ac.uk>
2015-09-04 10:17:05 +0000

    updates
    git-svn-id: http://proj.badc.rl.ac.uk/svn/exarch/CMIP6dreq/trunk@378 89a36f9e-84b7-11e1-99bb-b993851fa097

--- src/htmlTemplates.py
+++ src/htmlTemplates.py
@@ -19,3 +19,15 @@ indexWrapper = """
 item = """   <li>%(item)s</li>"""
 
 attribute = '''   <li><span title="%(keyTitle)s"><b>%(key)s</b></span>: %(value)s</li>'''
+
+tabItem = "<td>%s</td>"
+tabRow = "<tr>%s</tr>\n"
+table = """<table%(attributes)s>%(head)s%(content)s</table>"""
+
+fTabItem = '<td><input name="%s" value="%s" type="text"/></td>'
+fTabRow = "<tr>%s</tr>\n"
+fTable = """<form action="http://localhost:8051/" method="POST" name="myForm">
+ <input type="submit" value="Submit">
+ <input type="hidden" name="formid" value="%(formid)s">
+<table%(attributes)s>%(head)s%(fContent)s</table></form>"""
+
