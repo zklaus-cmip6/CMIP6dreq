4e2e0d8d67f120047f65e279ee677731b275d98b
Martin Juckes <martin.juckes@stfc.ac.uk>
2015-10-13 14:04:48 +0000

    adding __init__.py for pypi distribution
    git-svn-id: http://proj.badc.rl.ac.uk/svn/exarch/CMIP6dreq/trunk@413 89a36f9e-84b7-11e1-99bb-b993851fa097

---
+++ dreqPy/__init__.py
@@ -0,0 +1,10 @@
+
+
+__version__ = "01.beta.05"
+__title__ = "dreqPy"
+__description__ = "CMIP6 Data Request Python API"
+__uri__ = "http://proj.badc.rl.ac.uk/svn/exarch/CMIP6dreq/tags/{0}".format(__version__)
+__author__ = "Martin Juckes"
+__email__ = "martin.juckes@stfc.ac.uk"
+__license__ = "BSD"
+__copyright__ = "Copyright (c) 2015 Science & Technology Facilities Council (STFC)"
