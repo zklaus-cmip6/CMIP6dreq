a657a4d38966d56bf40ecb89caf0a302b33fa8e1
Martin Juckes <martin.juckes@stfc.ac.uk>
2016-11-09 10:14:24 +0000

    01.beta.41
    git-svn-id: http://proj.badc.rl.ac.uk/svn/exarch/CMIP6dreq/tags/01.beta.41@841 89a36f9e-84b7-11e1-99bb-b993851fa097

---
+++ trunk/dreqPy/packageConfig.py
@@ -0,0 +1,26 @@
+"""packageConfig
+-------------
+
+Basic information about the package, used by setup.py to populate package metadata.
+"""
+
+import os
+
+PACKAGE_DIR = os.path.dirname(os.path.abspath(__file__) )
+
+##DOC_DEFAULT_DIR
+DOC_DEFAULT_DIR = os.path.join(PACKAGE_DIR, 'docs')
+
+DOC_DIR = os.environ.get('DRQ_CONFIG_DIR', DOC_DEFAULT_DIR)
+
+__versionComment__ = "Updates to AerChemMIP experiment names; removed some variable duplication; cleaned uid strings"
+__version__ = "01.beta.41"
+__title__ = "dreqPy"
+__description__ = "CMIP6 Data Request Python API"
+__uri__ = "http://proj.badc.rl.ac.uk/svn/exarch/CMIP6dreq/tags/{0}".format(__version__)
+__author__ = "Martin Juckes"
+__email__ = "martin.juckes@stfc.ac.uk"
+__license__ = "BSD"
+__copyright__ = "Copyright (c) 2015 Science & Technology Facilities Council (STFC)"
+
+version = __version__
